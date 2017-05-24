c7742cd4cbba17194f4128442553e3f1e6730750
Shamil Fattakhov <voidproxy@gmail.com>
2017-05-24 17:08:49 +0000

    Update setup-wale.sh

--- setup-wale.sh
+++ setup-wale.sh
@@ -16,7 +16,8 @@ then
     rm -rf /tmp/pg-data
 
     # Create recovery.conf
-    echo "standby_mode     = 'yes'" > $PGDATA/recovery.conf
+    echo "hot_standby      = 'on'" > $PGDATA/recovery.conf
+    echo "standby_mode     = 'on'" >> $PGDATA/recovery.conf
     echo "restore_command  = 'envdir /etc/wal-e.d/env /usr/local/bin/wal-e wal-fetch "%f" "%p"'" >> $PGDATA/recovery.conf
     echo "trigger_file     = '$PGDATA/trigger'" >> $PGDATA/recovery.conf
 
