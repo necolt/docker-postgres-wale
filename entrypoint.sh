e469a1d4f3cce4375c0318ac7e54374735d84f4e
Shamil Fattakhov <voidproxy@gmail.com>
2017-05-24 15:57:49 +0000

    Make it work

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,15 @@
 set -e
 
 if [ "$1" = 'postgres' ]; then
+  # Assumption: the group is trusted to read secret information
+  umask u=rwx,g=rx,o=
+  mkdir -p /etc/wal-e.d/env
+
+  echo "$WALE_AWS_SECRET_ACCESS_KEY" > /etc/wal-e.d/env/AWS_SECRET_ACCESS_KEY
+  echo "$WALE_AWS_ACCESS_KEY_ID" > /etc/wal-e.d/env/AWS_ACCESS_KEY_ID
+  echo "$WALE_S3_PREFIX" > /etc/wal-e.d/env/WALE_S3_PREFIX
+  echo "$WALE_AWS_REGION" > /etc/wal-e.d/env/AWS_REGION
+  chown -R root:postgres /etc/wal-e.d
 
   if [ ! -s "$PGDATA/PG_VERSION" ]; then
     echo $PGDATA/PG_VERSION does not exist
--- setup-wale.sh
+++ setup-wale.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Assumption: the group is trusted to read secret information
-umask u=rwx,g=rx,o=
-mkdir -p /etc/wal-e.d/env
-
-echo "$WALE_AWS_SECRET_ACCESS_KEY" > /etc/wal-e.d/env/AWS_SECRET_ACCESS_KEY
-echo "$WALE_AWS_ACCESS_KEY_ID" > /etc/wal-e.d/env/AWS_ACCESS_KEY_ID
-echo "$WALE_S3_PREFIX" > /etc/wal-e.d/env/WALE_S3_PREFIX
-echo "$WALE_AWS_REGION" > /etc/wal-e.d/env/AWS_REGION
-chown -R root:postgres /etc/wal-e.d
-
 if [ "$POSTGRES_AUTHORITY" = "slave" ]
 then
   echo "Authority: Slave - Fetching latest backups";
@@ -18,22 +8,20 @@ then
     echo "wal-e already configured in /var/lib/postgresql/data/recovery.conf"
   else
     sleep 5
-    gosu postgres pg_ctl -D "$PGDATA" -w stop
+    pg_ctl -D "$PGDATA" -w stop
     # $PGDATA cannot be removed so use temporary dir
     # If you don't stop the server first, you'll waste 5hrs debugging why your WALs aren't pulled
-    su - postgres -c "envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-fetch /tmp/pg-data LATEST"
-    su - postgres -c "cp -rf /tmp/pg-data/* $PGDATA"
-    su - postgres -c "rm -rf /tmp/pg-data"
+    envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-fetch /tmp/pg-data LATEST
+    cp -rf /tmp/pg-data/* $PGDATA
+    rm -rf /tmp/pg-data
 
     # Create recovery.conf
-    echo "standby_mode     = 'yes'" >> $PGDATA/recovery.conf
+    echo "standby_mode     = 'yes'" > $PGDATA/recovery.conf
     echo "restore_command  = 'envdir /etc/wal-e.d/env /usr/local/bin/wal-e wal-fetch "%f" "%p"'" >> $PGDATA/recovery.conf
     echo "trigger_file     = '$PGDATA/trigger'" >> $PGDATA/recovery.conf
 
-    chown -R postgres "$PGDATA"
-
     # Starting server again to satisfy init script
-    gosu postgres pg_ctl -D "$PGDATA" -o "-c listen_addresses=''" -w start
+    pg_ctl -D "$PGDATA" -o "-c listen_addresses=''" -w start
   fi
 else
   echo "Authority: Master - Scheduling WAL backups";
@@ -47,6 +35,6 @@ else
     echo "archive_timeout = 60" >> /var/lib/postgresql/data/postgresql.conf
   fi
 
-  su - postgres -c "crontab -l | { cat; echo \"0 3 * * * /usr/bin/envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-push /var/lib/postgresql/data\"; } | crontab -"
-  su - postgres -c "crontab -l | { cat; echo \"0 4 * * * /usr/bin/envdir /etc/wal-e.d/env /usr/local/bin/wal-e delete --confirm retain 30\"; } | crontab -"
+  crontab -l | { cat; echo "0 3 * * * /usr/bin/envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-push /var/lib/postgresql/data"; } | crontab -
+  crontab -l | { cat; echo "0 4 * * * /usr/bin/envdir /etc/wal-e.d/env /usr/local/bin/wal-e delete --confirm retain 30"; } | crontab -
 fi
